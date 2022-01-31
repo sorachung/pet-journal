4c7585f0b2428d64316611c11692bb891612e871
sorachung <sorachung.src@gmail.com>
2022-01-31 11:08:38 -0600

    filled out user repository with relevant fetches

--- src/repositories/UserRepository.js
+++ src/repositories/UserRepository.js
@@ -1,6 +1,32 @@
-import Settings from "./Settings.js"
-import { fetchIt } from "./Fetch.js";
+import Settings from "./Settings"
+import { fetchIt } from "./Fetch"
 
 export default {
-    async
+    async get(id) {
+        return await fetchIt(`${Settings.remoteURL}/users/${id}`)
+    },
+    async getEmbedPets(id) {
+        return await fetchIt(`${Settings.remoteURL}/users/${id}&_embed=pets`)
+    },
+    async getEmbedContacts(id) {
+        return await fetchIt(`${Settings.remoteURL}/users/${id}&_embed=contacts`)
+    },
+    async createAccount(user) {
+        return await fetchIt(`${Settings.remoteURL}/users`, "POST", JSON.stringify(user))
+    },
+    async findUser(un) {
+        return await fetchIt(`${Settings.remoteURL}/users?email=${un}`)
+    },
+    async delete(id) {
+        return await fetchIt(`${Settings.remoteURL}/users/${id}`, "DELETE")
+    },
+    async getAll() {
+        return await fetchIt(`${Settings.remoteURL}/users`)
+    },
+    async getAllEmbedPets() {
+        return await fetchIt(`${Settings.remoteURL}/users&_embed=pets`)
+    },
+    async getAllEmContacts() {
+        return await fetchIt(`${Settings.remoteURL}/users&_embed=contacts`)
+    }
 }
