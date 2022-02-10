79de0bd24761e59fca0a2c1651d67d6152017b59
sorachung <sorachung.src@gmail.com>
2022-02-09 19:11:16 -0600

    fixed expanded properties being written to db

--- src/components/medicals/vetVisits/ViewInvoiceDialog.js
+++ src/components/medicals/vetVisits/ViewInvoiceDialog.js
@@ -2,23 +2,20 @@ import React, { useState } from "react";
 import MedicalRepository from "../../../repositories/MedicalRepository";
 
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
-import TextField from "@mui/material/TextField";
 
 export const ViewInvoiceDialog = ({ vetVisit, syncVetVisits, openInv, setOpenInv }) => {
-    const [newVetVisit, setNewVetVisit] = useState({ vetId: "" });
    
     const removeInvoice = () => {
         handleClose();
         const copy = {...vetVisit}
         copy.invoicePicURL = null;
+        delete copy.pet;
+        delete copy.vet
         MedicalRepository.editVetVisit(copy).then(() => syncVetVisits());
     }
 
@@ -33,7 +30,9 @@ export const ViewInvoiceDialog = ({ vetVisit, syncVetVisits, openInv, setOpenInv
             <Dialog open={openInv} onClose={handleClose} maxWidth={false}>
                     <DialogTitle>Inovice</DialogTitle>
                     <DialogContent>
-                        <img src={vetVisit.invoicePicURL} alt="vet visit invoice" />
+                        <Box sx={{maxWidth:"100%"}}>
+                            <img src={vetVisit.invoicePicURL} alt="vet visit invoice" />
+                        </Box>
                     </DialogContent>
                     <DialogActions>
 
