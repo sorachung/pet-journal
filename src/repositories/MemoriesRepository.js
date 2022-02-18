96069d717c5776c29c1b01669a869f69f9543ffd
sorachung <sorachung.src@gmail.com>
2022-02-18 16:40:36 -0600

    added fetch calls for memories

---
+++ src/repositories/MemoriesRepository.js
@@ -0,0 +1,40 @@
+import Settings from "./Settings"
+import { fetchIt } from "./Fetch"
+
+export default {
+    async get(id) {
+        return await fetchIt(`${Settings.remoteURL}/memories/${id}`)
+    },
+    async addMemory(memory) {
+        return await fetchIt(`${Settings.remoteURL}/memories`, "POST", JSON.stringify(memory))
+    },
+    async editMemory(memory) {
+        return await fetchIt(`${Settings.remoteURL}/memories/${memory.id}`, "PUT", JSON.stringify(memory))
+    },
+    async findMemoriesByUser(userId) {
+        return await fetchIt(`${Settings.remoteURL}/memories?userId=${userId}`)
+    },
+    async findStarredMemoriesByUser(userId) {
+        return await fetchIt(`${Settings.remoteURL}/memories?userId=${userId}&starred=true`)
+    },
+    async delete(id) {
+        return await fetchIt(`${Settings.remoteURL}/memories/${id}`, "DELETE")
+    },
+
+    //memoriesTags fetches
+    async getTags(id) {
+        return await fetchIt(`${Settings.remoteURL}/memoriesTags/${id}`)
+    },
+    async addTag(tag) {
+        return await fetchIt(`${Settings.remoteURL}/memoriesTags`, "POST", JSON.stringify(memory))
+    },
+    async editTag(tag) {
+        return await fetchIt(`${Settings.remoteURL}/memoriesTags/${tag.id}`, "PUT", JSON.stringify(tag))
+    },
+    async findTagsByPetExpandMemories(petId) {
+        return await fetchIt(`${Settings.remoteURL}/memoriesTags?userId=${petId}&_expand=memory`)
+    },
+    async deleteTags(id) {
+        return await fetchIt(`${Settings.remoteURL}/memoriesTags/${id}`, "DELETE")
+    },
+}
